94e8b3cef5cd588a16611d135ea47f39901b3c49
Aivirth <aivirth.work@gmail.com>
2018-02-21 22:56:55 +0100

    Basic routes

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 //initialize app
 const app = express();
+
+//index route
+app.get('/', (req, res)=>{
+    req.send('index');
+});
+
+//index route
+app.get('/about', (req, res)=>{
+    req.send('about');
+});
+
+
 const port = 5000;
 app.listen(port, ()=>{
     console.log(`App started at port ${port}`);
