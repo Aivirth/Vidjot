6706cf2264a75853d1227649a65c55f9d85782fb
Aivirth <aivirth.work@gmail.com>
2018-02-25 21:50:15 +0100

    Added handlebars template engine

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 //initialize app
 const app = express();
 
+//handlebars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+
 //index route
 app.get('/', (req, res)=>{
-    req.send('index');
+    const title = 'Index';
+    res.render('index', {
+        title : title
+    });
 });
 
 //index route
 app.get('/about', (req, res)=>{
-    req.send('about');
+    const title = 'About';
+    res.render('about', {
+        title : title
+    });
 });
 
 
