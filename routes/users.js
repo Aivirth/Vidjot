b845a12fdd4462399653fcb44c5e5fcbd9ac27cb
Aivirth <aivirth.work@gmail.com>
2018-02-28 20:45:53 +0100

    Added logout

--- routes/users.js
+++ routes/users.js
@@ -78,6 +78,13 @@ router.post('/login', (req, res, next)=>{
         failureRedirect : '/users/login',
         failureFlash : true
     })(req, res, next);
+});
+
+//Logout route
+router.get('/logout', (req, res)=>{
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
 })
 
 
